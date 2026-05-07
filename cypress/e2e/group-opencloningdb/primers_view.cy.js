1449687313e900ea6227efd34e73e7411fd54c6b
Manuel Lera-Ramirez <manulera14@gmail.com>
2026-05-07 12:52:34 +0100

    fix tests

--- cypress/e2e/group-opencloningdb/primers_view.cy.js
+++ cypress/e2e/group-opencloningdb/primers_view.cy.js
@@ -1,7 +1,7 @@
 import endpoints from '../../../packages/opencloningdb/src/endpoints';
 describe('Actions that can be perfomed by a view-only user on the Primers page', () => {
   it('should render and make the right search request', () => {
-    cy.intercept('GET', Cypress.getDbURL(endpoints.primers)).as('getPrimers');
+    cy.intercept('GET', Cypress.getDbURL(endpoints.primers, '*')).as('getPrimers');
     cy.e2eLogin('/primers', 'view-only-user@example.com', 'password');
     cy.wait('@getPrimers').then(({ response, request }) => {
       expect(request.query).to.deep.equal({page: "1", size: "25"});
@@ -30,7 +30,7 @@ describe('Actions that can be perfomed by a view-only user on the Primers page',
           cy.get('td').eq(4).should('have.text', primerWithTagsAndUID.sequence);
         });
     });
-    cy.intercept('GET', Cypress.getDbURL(endpoints.primers)).as('getPrimers2');
+    cy.intercept('GET', Cypress.getDbURL(endpoints.primers, '*')).as('getPrimers2');
     cy.setInputValue('UID', 'ML7', '[data-testid="primers-page"]');
     cy.setInputValue('Name', 'fwd_restriction_then_ligation', '[data-testid="primers-page"]');
     cy.clickMultiSelectOption('Tags', 'templateless_PCR', '[data-testid="primers-page"]');
@@ -45,7 +45,7 @@ describe('Actions that can be perfomed by a view-only user on the Primers page',
       cy.wrap(request.query).should('have.property', 'has_uid', 'true');
     });
     // Finally a query to verify that multiple tags are working, mock since no need
-    cy.intercept('GET', Cypress.getDbURL(endpoints.primers), { statusCode: 200, body: { items: [] } }).as('getPrimers3');
+    cy.intercept('GET', Cypress.getDbURL(endpoints.primers, '*'), { statusCode: 200, body: { items: [] } }).as('getPrimers3');
     cy.setAutocompleteValue('Tags', 'crispr_hdr', '[data-testid="primers-page"]');
     cy.get('button').contains('Search').click();
     cy.wait('@getPrimers3').then(({ response, request }) => {
@@ -57,7 +57,7 @@ describe('Actions that can be perfomed by a view-only user on the Primers page',
   });
   it('should set query params from the URL', () => {
     cy.e2eLogin('/primers', 'view-only-user@example.com', 'password');
-    cy.intercept('GET', Cypress.getDbURL(endpoints.primers), { statusCode: 200, body: { items: [] } }).as('getPrimers2');
+    cy.intercept('GET', Cypress.getDbURL(endpoints.primers, '*'), { statusCode: 200, body: { items: [] } }).as('getPrimers2');
     cy.intercept('GET', Cypress.getDbURL(endpoints.tags), { statusCode: 200, body:[{ id: 1, name: 'crispr_hdr' }, { id: 2, name: 'templateless_PCR' }] }).as('getTags');
     cy.visit('/primers?uid=ML7&name=fwd_restriction_then_ligation&tags=1&tags=2&has_uid=true');
 
@@ -76,7 +76,7 @@ describe('Actions that can be perfomed by a view-only user on the Primers page',
 
   });
   it('clicking on entry shows the detail page and allows to add to design tab', () => {
-    cy.intercept('GET', Cypress.getDbURL(endpoints.primers)).as('getPrimers');
+    cy.intercept('GET', Cypress.getDbURL(endpoints.primers, '*')).as('getPrimers');
     cy.e2eLogin('/primers', 'view-only-user@example.com', 'password');
     cy.wait('@getPrimers').then(({ response }) => {
       const primer = response.body.items.find((primer) => primer.name === 'fwd_restriction_then_ligation');
@@ -109,7 +109,7 @@ describe('Actions that can be perfomed by a view-only user on the Primers page',
     cy.get('[data-testid="resource-detail-header-title"] span').contains('No UID').should('exist');
   });
   it('clicking on add to design tab button adds primers to the design tab', () => {
-    cy.intercept('GET', Cypress.getDbURL(endpoints.primers)).as('getPrimers');
+    cy.intercept('GET', Cypress.getDbURL(endpoints.primers, '*')).as('getPrimers');
     cy.e2eLogin('/primers', 'view-only-user@example.com', 'password');
     cy.wait('@getPrimers').then(({ response }) => {
       cy.get('button').contains('Add to Design Tab').should('not.exist');
